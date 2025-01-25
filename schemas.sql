e499d91b3b014619c7b7aec93f78f673694860c5
Bhavan14 <bhavankumargkk@gmail.com>
2025-01-25 14:22:58 +0530

    Create schemas.sql

---
+++ schemas.sql
@@ -0,0 +1,12 @@
+create table netflixUserbase(User_ID int(30),
+	Subscription_Type varchar(30),	
+    Monthly_Revenue	int(10),
+    Join_Date varchar(30),	
+    Last_Payment_Date varchar(30),
+    Country varchar(30),
+	Age int(3),
+	Gender varchar(10),
+    Device	varchar(20),
+    Plan_Duration varchar(20));
+
+select * from netflixuserbase;
